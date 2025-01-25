eb711d6917d837e20b089ba6dd1af483cb37dded
pranavpo <pranavpoudyal031@gmail.com>
2025-01-25 19:00:03 +0545

    some maths

--- main.cpp
+++ main.cpp
@@ -1,38 +1,35 @@
-#include<iostream>
+#include <glm/vec3.hpp>                  // glm::vec3
+#include <glm/vec4.hpp>                  // glm::vec4
+#include <glm/mat4x4.hpp>                // glm::mat4
+#include <glm/ext/matrix_transform.hpp>  // glm::translate, glm::rotate, glm::scale
+#include <glm/ext/matrix_clip_space.hpp> // glm::perspective
+#include <glm/ext/scalar_constants.hpp>  // glm::pi
+#include <iostream>
+#include <glm/gtx/string_cast.hpp>
+// using namespace std;
 
-using namespace std;
+// glm::mat4 camera(float Translate, glm::vec2 const& Rotate)
+// {
 
-namespace first_space{
-    void func(){
-        cout << "Inside first namespace" << endl;
-    }
-}
+// }
 
-namespace second_space{
-    void func(){
-        cout << "Inside second function" << endl;
-    }
-}
+int main()
+{
+    glm::vec3 A(4.0f, 0.0f, 0.0f);
+    glm::vec3 B(0.0f, 4.0f, 0.0f);
+    glm::vec3 C = glm::cross(A,B);
+    // glm::vec3 result = A + B;
+    // // glm::mat4 C(1.0f);
+    // std::cout << "A " << glm::length(A) << std::endl;
+    // // std::cout << "B " << glm::length(B) << std::endl;
+    // std::cout << "A-normalized " << glm::to_string(glm::normalize(A)) << std::endl;
+    // std::cout << "Length of A after normalization: " << glm::length(glm::normalize(A)) << std::endl;
+    // std::cout << "Sum of A and B: " << glm::to_string(result) << std::endl;
+    // std::cout << "Dot product of A and B: " << glm::dot(normalize(A),normalize(B)) << std::endl;
+    // float dot_product_normalized = glm::dot(normalize(A),normalize(B));
+    // std::cout << "angle is: " << std::acos(dot_product_normalized) * 180.0/M_PI << std::endl;
 
-using namespace second_space;
+    std::cout << "Cross product AXB " << glm::to_string(C) << std::endl;
 
-int main(){
-    func();
-
-    int x;
-
-    cout << "type a number: " << endl;
-    cin >> x;
-    cout << "Your number is " << x << endl; 
-
-    string name = "pranav";
-
-    cout << "name :" << name << endl;
-
-    string firstName = "Joe";
-    string lastName = "Rogan";
-    string fullName = firstName + lastName;
-    cout << "full name: " << fullName << endl;  
-    cout << "reached here" <<endl;
     return 0;
 }
